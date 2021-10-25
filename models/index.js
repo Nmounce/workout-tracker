6e951d4a77fd9fc276112c8664f2db173a14fbbd
NikkiMounce <nikkimounce@gmail.com>
2021-10-25 16:51:09 -0700

    updated model index

--- models/index.js
+++ models/index.js
@@ -1,10 +1,3 @@
-const Workout = require('./workout');
-const Cardio = require('./cardio');
-const Resistance = require('./resistance');
-
-
 module.exports = {
-    Workout,
-    Cardio,
-    Resistance
+    Workout: require('./workout'),
 };
