1e8fe7fd4ef19100304d50c5e7ab4e25f91a2f5c
NikkiMounce <nikkimounce@gmail.com>
2021-10-25 17:51:37 -0700

    changed routes in api-route file

--- routes/api_route.js
+++ routes/api_route.js
@@ -1,37 +1,42 @@
 const db = require('../models');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
     //get last workout
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({})
             .then(workout => {
                 res.json(workout);
-        }).catch(err => {
-            res.json(err);
-        });
+            }).catch(err => {
+                res.json(err);
+            });
     });
 
     //create a new workout
-    app.post("/api/workouts", ({
-        body
-    }, res) => {
-        // console.log(body);
-        db.Workout.create(body).then((dbWorkout => {
-            res.json(dbWorkout);
-        })).catch(err => {
-            res.json(err);
-        });
-    });
+    app.post("/api/workouts", async (req, res) => {
+        try {
+            const response = await db.Workout.create({
+                type: "workout"
+            })
+            res.json(response);
+        } catch (err) {
+            console.log("An error occurred while creating your workout", err);
+        }
+    })
 
     //add an exercise to a workout
-    router.put("/api/workouts/:id", ({body, params}, res) => {
+    app.put("/api/workouts/:id", ({
+        body,
+        params
+    }, res) => {
         console.log(body, params);
         const workoutId = params.id;
         let savedExercises = [];
 
         //get all saved exercises in current workout
-        db.Workout.find({ id: workoutId })
+        db.Workout.find({
+                id: workoutId
+            })
             .then(dbWorkout => {
                 savedExercises = dbWorkout[0].exercises;
                 res.json(dbWorkout[0].exercises);
@@ -42,14 +47,23 @@ module.exports = function(app) {
                 res.json(err);
             });
 
+        function updateWorkout(exercises) {
+            db.Workout.findByIdAndUpdate(workoutId, {
+                exercises: exercises
+            }, function (err, doc) {
+                if (err) {
+                    console.log(err);
+                }
+            })
+        }
+    })
     //get workout data in specific date range
     router.get("/api/workouts/range", (req, res) => {
-        db.Workout.find({}).then(dbWorkout => {
-            console.log("All Workouts");
-            console.log(dbWorkout);
-            res.json(dbWorkout);
-        }).catch(err => {
-            res.json(err);
-        });
+        db.Workout.find({})
+            .then(workout => {
+                res.json(workout);
+            }).catch(err => {
+                res.json(err);
+            });
     });
-
+};
