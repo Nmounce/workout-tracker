a943a726d22c9cbb9b4d90019c18a66eb7d76cb0
NikkiMounce <nikkimounce@gmail.com>
2021-10-25 18:00:26 -0700

    updated workout model

--- models/workout.js
+++ models/workout.js
@@ -2,21 +2,26 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  date: {
+const workoutSchema = new Schema({
+  day: {
     type: Date,
     default: Date.now
   },
-  exercise: [{
+  exercises: [{
     type: {
       type: String,
       trim: true,
+      required: "Enter an exercise type"
     },
     name: {
       type: String,
       trim: true,
+      required: "Enter the exercise name"
     },
-    duration: Number,
+    duration: {
+      type: Number,
+      required: "Enter the exercise duration in minutes"
+    },
     weight: {
       type: Number,
       default: 0
@@ -28,14 +33,25 @@ const WorkoutSchema = new Schema({
     sets: {
       type: Number,
       default: 0
+    },
+    distance: {
+      type: Number,
+      default: 0
     }
-  }],
-  totalDuration: {
-    type: Number,
-    default: 0
+  }]
+}, {
+  toJSON: {
+    virtual: true,
   }
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+//add a dynamically created property to the schema object
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
