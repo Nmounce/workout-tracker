aae9f23ad375977510f59c90cbcaa198275e5edc
NikkiMounce <nikkimounce@gmail.com>
2021-10-25 17:01:43 -0700

    modified server.js

--- server.js
+++ server.js
@@ -1,116 +1,29 @@
+const express = require('express');
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const path = require("path");
 
-const db = require("./models");
+const PORT = process.env.PORT || 3002;
+
 const app = express();
 
 app.use(logger("dev"));
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreatedIndexes: true,
+        useFindAndModify: false,
+    });
 
-const databaseUrl = "workout_tracker";
-const collections = ["exercise"];
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
-const db = mongojs(databaseUrl, collections);
-
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "./public/index.html"));
-});
-
-app.post("/submit", (req, res) => {
-  console.log(req.body);
-
-  db.exercise.insert(req.body, (error, data) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-app.get("/all", (req, res) => {
-  db.exercise.find({}, (error, data) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.json(data);
-    }
-  });
-});
-
-app.get("/find/:id", (req, res) => {
-  db.exercise.findOne(
-    {
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-app.post("/update/:id", (req, res) => {
-  db.exercise.update(
-    {
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    {
-      $set: {
-        title: req.body.title,
-        note: req.body.note,
-        modified: Date.now()
-      }
-    },
-    (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-// app.delete("/delete/:id", (req, res) => {
-//   db.exercise.remove(
-//     {
-//       _id: mongojs.ObjectID(req.params.id)
-//     },
-//     (error, data) => {
-//       if (error) {
-//         res.send(error);
-//       } else {
-//         res.send(data);
-//       }
-//     }
-//   );
-// });
-
-// app.delete("/clearall", (req, res) => {
-//   db.exercise.remove({}, (error, response) => {
-//     if (error) {
-//       res.send(error);
-//     } else {
-//       res.send(response);
-//     }
-//   });
-// });
-
-app.listen(3000, () => {
-  console.log("App running on port 3000!");
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
 });
